d59605b92548ea448c030b606bef749c6dfc8a01
Param Aggarwal <paramaggarwal@gmail.com>
2017-03-31 22:54:53 +0530

    Add delta check on time

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -145,7 +145,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 			   dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
 			   0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
 
-  ekf_.Predict();
+  if (dt > 0.1) {
+    ekf_.Predict();
+  }
 
   /*****************************************************************************
    *  Update
