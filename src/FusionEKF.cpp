ac621ac6ecc31f45c6d7e6ec84bfaa36c48b21e8
Param Aggarwal <paramaggarwal@gmail.com>
2017-03-31 22:54:06 +0530

    Update position initialisation

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -61,7 +61,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     // first measurement
     // cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
-    ekf_.x_ << 1, 1, 1, 1;
+    ekf_.x_ << 0, 0, 0, 0;
 
     //state covariance matrix P
     ekf_.P_ = MatrixXd(4, 4);
